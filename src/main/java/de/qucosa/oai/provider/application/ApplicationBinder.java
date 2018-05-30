9969c557b1b9a99894adb6dbc1a2de43fb8940ef
dseelig <daniel.seelig@slub-dresden.de>
2018-05-30 09:23:17 +0200

    Rename identifier service to record service.

--- src/main/java/de/qucosa/oai/provider/application/ApplicationBinder.java
+++ src/main/java/de/qucosa/oai/provider/application/ApplicationBinder.java
@@ -19,7 +19,7 @@ package de.qucosa.oai.provider.application;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 import de.qucosa.oai.provider.persistence.postgres.FormatService;
-import de.qucosa.oai.provider.persistence.postgres.IndentifierService;
+import de.qucosa.oai.provider.persistence.postgres.RecordService;
 import de.qucosa.oai.provider.persistence.postgres.SetService;
 
 public class ApplicationBinder extends AbstractBinder {
@@ -27,7 +27,7 @@ public class ApplicationBinder extends AbstractBinder {
     @Override
     protected void configure() {
         bind(SetService.class).to(SetService.class);
-        bind(IndentifierService.class).to(IndentifierService.class);
+        bind(RecordService.class).to(RecordService.class);
         bind(FormatService.class).to(FormatService.class);
     }
 }
