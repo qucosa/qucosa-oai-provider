0ea1db7478e182fcbb3fbd8ca9456d19e8334e43
dseelig <daniel.seelig@slub-dresden.de>
2018-06-15 15:14:54 +0200

    Opzimize imports.

--- src/main/java/de/qucosa/oai/provider/persistence/pojos/Dissemination.java
+++ src/main/java/de/qucosa/oai/provider/persistence/pojos/Dissemination.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
-import java.sql.Date;
 import java.sql.Timestamp;
 
 @JsonAutoDetect
