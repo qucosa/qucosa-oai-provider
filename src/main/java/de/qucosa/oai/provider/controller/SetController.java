966a08bd4d6329760692f2ba11139580d31e1311
dseelig <daniel.seelig@slub-dresden.de>
2018-06-19 15:48:21 +0200

    Edit the save dao method call. Replace update with create.

--- src/main/java/de/qucosa/oai/provider/controller/SetController.java
+++ src/main/java/de/qucosa/oai/provider/controller/SetController.java
@@ -56,7 +56,7 @@ public class SetController {
             return Response.status(Response.Status.BAD_REQUEST).entity("The set mapping object is failed!").build();
         }
 
-        Set<de.qucosa.oai.provider.persistence.pojos.Set> result = setDao.update(saveRes);
+        Set<de.qucosa.oai.provider.persistence.pojos.Set> result = setDao.create(saveRes);
 
         return Response.status(Response.Status.OK).entity(result).build();
     }
