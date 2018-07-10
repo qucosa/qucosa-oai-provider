b0990ec8201b3f495526e95df00df4d500c165e4
dseelig <daniel.seelig@slub-dresden.de>
2018-07-10 11:20:41 +0200

    Remove old projct structure and classes. Add spring boot application main class.

---
+++ src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
@@ -0,0 +1,12 @@
+package de.qucosa.oai.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QucosaOaiProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QucosaOaiProviderApplication.class, args);
+    }
+}
