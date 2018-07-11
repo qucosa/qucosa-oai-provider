c6fefebb134943f7fa7c9b8d3a7bbf4352d03797
dseelig <daniel.seelig@slub-dresden.de>
2018-07-11 16:00:35 +0200

    Add enable auto configuration annotation

--- src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
+++ src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
@@ -1,9 +1,11 @@
 package de.qucosa.oai.provider;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class QucosaOaiProviderApplication {
 
     public static void main(String[] args) {
