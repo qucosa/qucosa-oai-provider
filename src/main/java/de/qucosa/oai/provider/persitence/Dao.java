fa23f260ccb3a895e2b77bfdfc5447e7a4df453a
dseelig <daniel.seelig@slub-dresden.de>
2018-07-11 10:57:38 +0200

    Implements dao methods

--- src/main/java/de/qucosa/oai/provider/persitence/Dao.java
+++ src/main/java/de/qucosa/oai/provider/persitence/Dao.java
@@ -1,5 +1,21 @@
 package de.qucosa.oai.provider.persitence;
 
-public interface Dao<T> {
+import java.util.Collections;
 
+public interface Dao<T> {
+    public T save(T object);
+
+    public T save(Collections objects);
+
+    public T update(T object);
+
+    public T update(Collections objects);
+
+    public T findAll();
+
+    public T findById(T value);
+
+    public T findByColumnAndValue(String column, T value);
+
+    public T delete(String column, T value);
 }
--- src/main/java/de/qucosa/oai/provider/persitence/dao/postgres/SetDao.java
+++ src/main/java/de/qucosa/oai/provider/persitence/dao/postgres/SetDao.java
@@ -3,6 +3,47 @@ package de.qucosa.oai.provider.persitence.dao.postgres;
 import de.qucosa.oai.provider.persitence.Dao;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
+
 @Repository
 public class SetDao<T> implements Dao<T> {
+    @Override
+    public T save(T object) {
+        return null;
+    }
+
+    @Override
+    public T save(Collections objects) {
+        return null;
+    }
+
+    @Override
+    public T update(T object) {
+        return null;
+    }
+
+    @Override
+    public T update(Collections objects) {
+        return null;
+    }
+
+    @Override
+    public T findAll() {
+        return null;
+    }
+
+    @Override
+    public T findById(T value) {
+        return null;
+    }
+
+    @Override
+    public T findByColumnAndValue(String column, T value) {
+        return null;
+    }
+
+    @Override
+    public T delete(String column, T value) {
+        return null;
+    }
 }
