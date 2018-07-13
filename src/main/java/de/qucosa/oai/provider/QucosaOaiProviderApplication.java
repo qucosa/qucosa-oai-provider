7aec1f9ff0d1eeb672ecd8a23954a993621b9c25
dseelig <daniel.seelig@slub-dresden.de>
2018-07-13 14:59:55 +0200

    Implements command line runner and this run method

--- src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
+++ src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
@@ -1,14 +1,24 @@
 package de.qucosa.oai.provider;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class QucosaOaiProviderApplication {
+public class QucosaOaiProviderApplication implements CommandLineRunner {
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
-        SpringApplication.run(QucosaOaiProviderApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(QucosaOaiProviderApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) { }
 }
