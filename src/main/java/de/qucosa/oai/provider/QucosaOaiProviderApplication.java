634d2ad0a4cd3fe8919fef586c432c6d67d10ada
dseelig <daniel.seelig@slub-dresden.de>
2018-07-16 15:25:16 +0200

    Add two annotations for load configs. Remove data source autowire.

--- src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
+++ src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
@@ -1,19 +1,18 @@
 package de.qucosa.oai.provider;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-import javax.sql.DataSource;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@PropertySource("classpath:application.properties")
+@ImportResource({"classpath:applicationContext.xml"})
 public class QucosaOaiProviderApplication implements CommandLineRunner {
-    @Autowired
-    private DataSource dataSource;
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(QucosaOaiProviderApplication.class, args);
