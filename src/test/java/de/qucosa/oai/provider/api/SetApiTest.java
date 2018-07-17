2f237775973b073b19f15c3eea0f63fd41fcc887
dseelig <daniel.seelig@slub-dresden.de>
2018-07-17 15:46:38 +0200

    Remove all integration test definitions. Change variables definitions. Change set api handling. Change method name for save set collection test.

--- src/test/java/de/qucosa/oai/provider/api/SetApiTest.java
+++ src/test/java/de/qucosa/oai/provider/api/SetApiTest.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.qucosa.oai.provider.api.sets.SetApi;
 import de.qucosa.oai.provider.config.ApplicationConfig;
 import de.qucosa.oai.provider.persitence.Dao;
-import de.qucosa.oai.provider.persitence.dao.postgres.SetDao;
 import de.qucosa.oai.provider.persitence.model.Set;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,46 +12,37 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import testdata.TestData;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = {ApplicationConfig.class})
 public class SetApiTest {
-    private static String sets;
-
-    private static String set;
-
     private SetApi setApi;
 
-//    @Autowired
-//    private ComboPooledDataSource dataSource;
-
-    @Autowired
-    private SetDao setDao;
+    private List<Set> sets = null;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Before
-    public void init() throws SQLException {
-        set = "{\"setspec\": \"ddc:1200\", \"setname\": \"Test Set 1200\", \"setdescription\" : \"\"}";
-
-        sets = "[{\"setspec\": \"ddc:1200\", \"setname\": \"Test Set 1200\", \"setdescription\" : \"\"}," +
-                "{\"setspec\": \"ddc:1201\", \"setname\": \"Test Set 1201\", \"setdescription\" : \"\"}]";
+    public void init() throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        sets = om.readValue(TestData.SETS, om.getTypeFactory().constructCollectionType(List.class, Set.class));
     }
 
     @Test
     public void Delivery_one_set_string_on_api() throws IOException {
-        setApi = new SetApi(set);
+        setApi = new SetApi(sets.get(0));
         Set data = (Set) setApi.getInputData();
         Assert.assertNotNull(data);
         Assert.assertNotNull(data.getSetSpec());
@@ -70,31 +60,36 @@ public class SetApiTest {
     @Test
     public void Save_set_object() throws IOException, SQLException {
         ObjectMapper om = new ObjectMapper();
-//        setDao.setConnection(dataSource);
-        setApi = new SetApi(new SetTestDao<>());
-        Set data = setApi.saveSet(om.readValue(set, Set.class));
+        setApi = new SetApi();
+        setApi.setDao(new SetTestDao<>());
+        Set data = setApi.saveSet(sets.get(0));
         Assert.assertNotNull(data);
         Assert.assertEquals(new Long(1), data.getSetId());
     }
 
     @Test
+    public void Save_multible_set_collection() throws SQLException {
+        setApi = new SetApi();
+        setApi.setDao(new SetTestDao());
+        List<Set> data = setApi.saveSets(sets);
+        Assert.assertEquals(2, data.size());
+    }
+
+    @Test
     public void Find_set_by_setspec_column() throws IOException, SQLException {
         ObjectMapper om = new ObjectMapper();
-        setApi = new SetApi(new SetTestDao<>(), om.readValue(set, Set.class));
+        setApi = new SetApi(sets);
+        setApi.setDao(new SetTestDao<>());
         Set set = setApi.find("setspec", "ddc:1200");
         Assert.assertEquals("ddc:1200", set.getSetSpec());
     }
 
     @Test
     public void Delete_set_by_column_and_value() throws SQLException {
-        setApi = new SetApi(new SetTestDao<>());
+        setApi = new SetApi();
+        setApi.setDao(new SetTestDao<>());
         Long setId = setApi.deleteSet("setspec", "ddc:1200");
         Assert.assertEquals(Long.valueOf(1), setId);
-    }
-
-    @Test
-    public void Update_set_by_setspec() {
-
     }
 
     private static class SetTestDao<T> implements Dao<T> {
@@ -107,8 +102,16 @@ public class SetApiTest {
         }
 
         @Override
-        public T save(Collections objects) {
-            return null;
+        public T save(Collection objects) {
+            int i = 0;
+
+            for (Iterator iterator = objects.iterator(); iterator.hasNext();) {
+                i++;
+                Set set = (Set) iterator.next();
+                set.setSetId(Long.valueOf(i));
+            }
+
+            return (T) objects;
         }
 
         @Override
@@ -117,7 +120,7 @@ public class SetApiTest {
         }
 
         @Override
-        public T update(Collections objects) {
+        public T update(Collection objects) {
             return null;
         }
 
@@ -138,7 +141,7 @@ public class SetApiTest {
             List<Set> sets = null;
 
             try {
-                sets = om.readValue(SetApiTest.sets, om.getTypeFactory().constructCollectionType(List.class, Set.class));
+                sets = om.readValue(TestData.SETS, om.getTypeFactory().constructCollectionType(List.class, Set.class));
             } catch (IOException e) { }
 
             for (Set obj : sets) {
@@ -158,7 +161,7 @@ public class SetApiTest {
             Set set = null;
 
             try {
-                JsonNode nodes = om.readTree(SetApiTest.sets);
+                JsonNode nodes = om.readTree(TestData.SETS);
 
                 for (JsonNode entry : nodes) {
 
@@ -166,13 +169,11 @@ public class SetApiTest {
                         throw new SQLException("Set mark as deleted failed, no rwos affected.");
                     }
 
-                    if (!entry.get(column).equals(value)) {
-                        throw new SQLException("Set mark as deleted failed, no rwos affected.");
+                    if (entry.get(column).asText().equals(value)) {
+                        set = om.readValue(entry.toString(), Set.class);
+                        set.setSetId(new Long(1));
+                        set.setDeleted(true);
                     }
-
-                    set = om.readValue(entry.toString(), Set.class);
-                    set.setSetId(new Long(1));
-                    set.setDeleted(true);
                 }
             } catch (IOException e) { }
 
