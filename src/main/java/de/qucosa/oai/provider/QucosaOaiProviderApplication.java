d936cf6df3c87b0c2b376b35c80556c091ebd879
dseelig <daniel.seelig@slub-dresden.de>
2018-07-18 15:55:23 +0200

    Remove auto config annotation.

--- src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
+++ src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
@@ -2,14 +2,12 @@ package de.qucosa.oai.provider;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @PropertySource("classpath:application.properties")
 @ImportResource({"classpath:applicationContext.xml"})
 public class QucosaOaiProviderApplication implements CommandLineRunner {
