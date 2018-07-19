4092e5cef9f4479d3c361292583d104286d34b6c
dseelig <daniel.seelig@slub-dresden.de>
2018-07-19 11:09:38 +0200

    Implements format controller tests

--- src/test/java/de/qucosa/oai/provider/controller/FormatControllerTest.java
+++ src/test/java/de/qucosa/oai/provider/controller/FormatControllerTest.java
@@ -1,4 +1,132 @@
 package de.qucosa.oai.provider.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.qucosa.oai.provider.api.format.FormatApi;
+import de.qucosa.oai.provider.dao.FormatTestDao;
+import de.qucosa.oai.provider.persitence.Dao;
+import de.qucosa.oai.provider.persitence.model.Format;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import testdata.TestData;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class FormatControllerTest {
+
+    private List<Format> formats = null;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws IOException {
+        formats = om.readValue(TestData.FORMATS, om.getTypeFactory().constructCollectionType(List.class, Format.class));
+    }
+
+    @TestPropertySource("classpath:application.properties")
+    @TestConfiguration
+    public static class FormatControllerTestConfiguration {
+        @Bean
+        public <T> Dao<T> formatDao() {
+            return new FormatTestDao<>();
+        }
+
+        @Bean
+        public FormatApi formatApi() {
+            FormatApi formatApi = new FormatApi();
+            formatApi.setDao(formatDao());
+            return formatApi;
+        }
+    }
+
+    @Test
+    public void Save_single_format_object() throws Exception {
+        Format format = formats.get(0);
+        mvc.perform(post("/formats")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(format)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.formatid", is(1)));
+    }
+
+    @Test
+    public void Save_format_collection() throws Exception {
+        mvc.perform(post("/formats")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(formats)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)));
+    }
+
+    @Test
+    public void Update_format() throws Exception {
+        Format format = formats.get(0);
+        format.setNamespace("mist");
+        mvc.perform(put("/formats/oai_dc")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(format)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.namespace", is("mist")));
+    }
+
+    @Test
+    public void Find_format_by_mdprefix() throws Exception {
+        mvc.perform(get("/formats/oai_dc")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.mdprefix", is("oai_dc")));
+    }
+
+    @Test
+    public void Find_all_formats() throws Exception {
+        mvc.perform(get("/formats")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)));
+    }
+
+    @Test
+    public void Mark_format_as_delete() throws Exception {
+        mvc.perform(delete("/formats/oai_dc/true")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.mdprefix", is("oai_dc")))
+                .andExpect(jsonPath("$.deleted", is(true)));
+    }
+
+    @Test
+    public void Mark_format_as_not_delete() throws Exception {
+        mvc.perform(delete("/formats/oai_dc/false")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.mdprefix", is("oai_dc")))
+                .andExpect(jsonPath("$.deleted", is(false)));
+    }
 }
