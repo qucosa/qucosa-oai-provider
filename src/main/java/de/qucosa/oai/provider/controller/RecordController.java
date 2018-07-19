fe87a7c79837443510b2c5219a7fb50bd8438c44
dseelig <daniel.seelig@slub-dresden.de>
2018-07-19 15:44:08 +0200

    Implement code for save an complete record from transport data. Inject objects.

--- src/main/java/de/qucosa/oai/provider/controller/RecordController.java
+++ src/main/java/de/qucosa/oai/provider/controller/RecordController.java
@@ -1,9 +1,12 @@
 package de.qucosa.oai.provider.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.qucosa.oai.provider.api.record.RecordApi;
 import de.qucosa.oai.provider.persitence.Dao;
+import de.qucosa.oai.provider.persitence.model.Format;
 import de.qucosa.oai.provider.persitence.model.RecordTransport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@RequestMapping("/record")
+import java.io.IOException;
+import java.util.List;
+
+@RequestMapping("/records")
 @RestController
 public class RecordController {
     @Autowired
@@ -22,9 +29,32 @@ public class RecordController {
     @Autowired
     private RecordApi recordApi;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private Environment environment;
+
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity save(@RequestBody RecordTransport input) {
+    public ResponseEntity save(@RequestBody String input) {
+        ObjectMapper om = new ObjectMapper();
+
+        try {
+            List<RecordTransport> inputData = om.readValue(input, om.getTypeFactory().constructCollectionType(List.class, RecordTransport.class));
+
+            if (inputData == null || inputData.size() == 0) {
+                return new ResponseEntity("Record transport mapping failed.", HttpStatus.BAD_REQUEST);
+            }
+
+            for (RecordTransport rt : inputData) {
+                Format format = restTemplate.getForObject(environment.getProperty("app.url") + "/formats/" + rt.getFormat().getMdprefix(), Format.class);
+                format.getMdprefix();
+            }
+        } catch (IOException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
         return new ResponseEntity(HttpStatus.OK);
     }
 
