6f2cce0902d13b67cfb8f4c270b3598e1799bf1d
dseelig <daniel.seelig@slub-dresden.de>
2018-07-19 14:29:52 +0200

    Remove all methods until save method. Add record api injection.

--- src/main/java/de/qucosa/oai/provider/controller/RecordController.java
+++ src/main/java/de/qucosa/oai/provider/controller/RecordController.java
@@ -1,13 +1,12 @@
 package de.qucosa.oai.provider.controller;
 
+import de.qucosa.oai.provider.api.record.RecordApi;
 import de.qucosa.oai.provider.persitence.Dao;
-import de.qucosa.oai.provider.persitence.model.Record;
 import de.qucosa.oai.provider.persitence.model.RecordTransport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,27 +19,14 @@ public class RecordController {
     @Autowired
     private Dao recordDao;
 
+    @Autowired
+    private RecordApi recordApi;
+
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity save(@RequestBody RecordTransport input) {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(value = "{uid}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity<Record> update(@RequestBody RecordTransport input, @PathVariable String uid) {
-        return new ResponseEntity(new Record(), HttpStatus.OK);
-    }
 
-    @RequestMapping(value = "{uid}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity<Record> delete(@PathVariable String uid) {
-        return new ResponseEntity(new Record(), HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "{uid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity<Record> find(@PathVariable String uid) {
-        return new ResponseEntity(new Record(), HttpStatus.OK);
-    }
 }
