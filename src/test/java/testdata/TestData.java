b22f017beecd3ac3294f472570ea5e24c70b6f2e
dseelig <daniel.seelig@slub-dresden.de>
2018-07-19 08:30:24 +0200

    Add formats test data objects

--- src/test/java/testdata/TestData.java
+++ src/test/java/testdata/TestData.java
@@ -1,6 +1,11 @@
 package testdata;
 
 public class TestData {
+
     public static String SETS = "[{\"setspec\": \"ddc:1200\", \"setname\": \"Test Set 1200\", \"setdescription\" : \"\"}," +
             "{\"setspec\": \"ddc:1201\", \"setname\": \"Test Set 1201\", \"setdescription\" : \"\"}]";
+
+    public static String FORMATS = "[{\"mdprefix\" : \"oai_dc\", \"schemaurl\" : \"http://www.openarchives.org/OAI/2.0/oai_dc/\", \"namespace\" : \"oai_dc\", \"deleted\" : \"false\"}," +
+            "{\"mdprefix\" : \"xmetadiss\", \"schemaurl\" : \"http://www.d-nb.de/standards/xmetadissplus/\", \"namespace\" : \"xmetadiss\", \"deleted\" : \"false\"}," +
+            "{\"mdprefix\" : \"epicur\", \"schemaurl\" : \"http://www.persistent-identifier.de/xepicur/version1.0/xepicur.xsd\", \"namespace\" : \"epicur\", \"deleted\" : \"false\"}]";
 }
