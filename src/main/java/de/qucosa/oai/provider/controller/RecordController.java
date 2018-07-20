359d9dccce0a88c6460cad957fe67099e18fe893
dseelig <daniel.seelig@slub-dresden.de>
2018-07-20 14:19:25 +0200

    Autowire set, format and  dissemination api. Implement record and dissemination find or save in save method. Add find record method.

--- src/main/java/de/qucosa/oai/provider/controller/RecordController.java
+++ src/main/java/de/qucosa/oai/provider/controller/RecordController.java
@@ -1,15 +1,18 @@
 package de.qucosa.oai.provider.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.qucosa.oai.provider.api.dissemination.DisseminationApi;
+import de.qucosa.oai.provider.api.format.FormatApi;
 import de.qucosa.oai.provider.api.record.RecordApi;
-import de.qucosa.oai.provider.persitence.Dao;
+import de.qucosa.oai.provider.api.sets.SetApi;
 import de.qucosa.oai.provider.persitence.model.Format;
+import de.qucosa.oai.provider.persitence.model.Record;
 import de.qucosa.oai.provider.persitence.model.RecordTransport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,17 +26,18 @@ import java.util.List;
 @RequestMapping("/records")
 @RestController
 public class RecordController {
-    @Autowired
-    private Dao recordDao;
 
     @Autowired
     private RecordApi recordApi;
 
     @Autowired
-    private Environment environment;
+    private FormatApi formatApi;
 
     @Autowired
-    private FormatsController formatsController;
+    private SetApi setApi;
+
+    @Autowired
+    private DisseminationApi disseminationApi;
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -51,14 +55,43 @@ public class RecordController {
                 Format format = null;
 
                 try {
-                    ResponseEntity<Format> formatRE = formatsController.find(rt.getFormat().getMdprefix());
-                    format = formatRE.getBody();
+                    format = formatApi.find("mdprefix", rt.getFormat().getMdprefix());
                 } catch (SQLException e) {
-                    ResponseEntity<Format> formatRE = formatsController.save(om.writeValueAsString(rt.getFormat()));
-                    format = formatRE.getBody();
+
+                    try {
+                        format = formatApi.saveFormat(rt.getFormat());
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
                 }
 
+                Record record = null;
 
+                try {
+                    record = recordApi.findRecord("uid", rt.getRecord().getUid());
+                } catch (SQLException e) {
+
+                    try {
+                        record = recordApi.saveRecord(rt.getRecord());
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+                try {
+                    setApi.saveSets(rt.getSets());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                rt.getDissemination().setFormatId(format.getFormatId());
+                rt.getDissemination().setRecordId(record.getRecordId());
+
+                try {
+                    disseminationApi.saveDissemination(rt.getDissemination());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         } catch (IOException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
@@ -67,5 +100,17 @@ public class RecordController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @RequestMapping(value = "{uid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<Record> find(@PathVariable String uid) {
+        Record record = null;
 
+        try {
+            record = recordApi.findRecord("uid", uid);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<Record>(record, HttpStatus.OK);
+    }
 }
