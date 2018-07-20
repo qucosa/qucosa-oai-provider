24aba868c25fce45288f6e8f666887045ea75c63
dseelig <daniel.seelig@slub-dresden.de>
2018-07-20 14:21:45 +0200

    Implements crud methods.

--- src/test/java/de/qucosa/oai/provider/dao/RecordTestDao.java
+++ src/test/java/de/qucosa/oai/provider/dao/RecordTestDao.java
@@ -1,9 +1,13 @@
 package de.qucosa.oai.provider.dao;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import de.qucosa.oai.provider.persitence.Dao;
 import de.qucosa.oai.provider.persitence.model.Record;
+import testdata.TestData;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
@@ -11,7 +15,9 @@ import java.util.List;
 public class RecordTestDao<Tparam> implements Dao<Record, Tparam> {
     @Override
     public Record save(Tparam object) throws SQLException {
-        return null;
+        Record record = (Record) object;
+        record.setRecordId(Long.valueOf(1));
+        return record;
     }
 
     @Override
@@ -31,7 +37,15 @@ public class RecordTestDao<Tparam> implements Dao<Record, Tparam> {
 
     @Override
     public List<Record> findAll() throws SQLException {
-        return null;
+        ObjectMapper om = new ObjectMapper();
+        List<Record> records = null;
+
+        try {
+            records = om.readValue(TestData.RECORDS, om.getTypeFactory().constructCollectionType(List.class, Record.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return records;
     }
 
     @Override
@@ -46,11 +60,60 @@ public class RecordTestDao<Tparam> implements Dao<Record, Tparam> {
 
     @Override
     public Record findByColumnAndValue(String column, Tparam value) throws SQLException {
+        ObjectMapper om = new ObjectMapper();
+        Record record = null;
+
+        try {
+            JsonNode jsonNodes = om.readTree(TestData.RECORDS);
+            int i = 0;
+
+            for (JsonNode node : jsonNodes) {
+                i++;
+
+                if (!node.has(column)) {
+                    throw new SQLException(column + " not found in records table.");
+                }
+
+                if (node.get(column).asText().equals(value)) {
+                    record = om.readValue(node.toString(), Record.class);
+                    record.setRecordId(Long.valueOf(i));
+                    return record;
+                }
+            }
+        } catch (IOException e) {
+            return null;
+        }
+
         return null;
     }
 
     @Override
     public Record delete(String column, Tparam ident, boolean value) throws SQLException {
+        ObjectMapper om = new ObjectMapper();
+        Record record = null;
+
+        try {
+            JsonNode jsonNodes = om.readTree(TestData.RECORDS);
+            int i = 0;
+
+            for (JsonNode node : jsonNodes) {
+                i++;
+
+                if (!node.has(column)) {
+                    throw new SQLException(column + " not found in records table.");
+                }
+
+                if (node.get(column).asText().equals(ident)) {
+                    record = om.readValue(node.toString(), Record.class);
+                    record.setRecordId(Long.valueOf(i));
+                    record.setDeleted(value);
+                    return record;
+                }
+            }
+        } catch (IOException e) {
+            return null;
+        }
+
         return null;
     }
 }
