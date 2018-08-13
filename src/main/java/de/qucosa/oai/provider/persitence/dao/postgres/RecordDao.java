f51e52667751561f51b98c50882311224f495a09
dseelig <daniel.seelig@slub-dresden.de>
2018-08-13 15:12:54 +0200

    Replace null init with record object instance.

--- src/main/java/de/qucosa/oai/provider/persitence/dao/postgres/RecordDao.java
+++ src/main/java/de/qucosa/oai/provider/persitence/dao/postgres/RecordDao.java
@@ -95,7 +95,7 @@ public class RecordDao<Tparam> implements Dao<Record, Tparam> {
 
     @Override
     public Record findByColumnAndValue(String column, Tparam value) throws SQLException {
-        Record record = null;
+        Record record = new Record();
         String sql = "SELECT * FROM records WHERE " + column + " = ?";
         PreparedStatement ps = connection.prepareStatement(sql);
         ps.setString(1, (String) value);
