2f0dea04919a1957393837e44ebda0ce01580382
dseelig <daniel.seelig@slub-dresden.de>
2018-09-11 15:09:42 +0200

    Add constructor with string and exception object argument.

--- src/main/java/de/qucosa/oai/provider/persitence/exceptions/NotFound.java
+++ src/main/java/de/qucosa/oai/provider/persitence/exceptions/NotFound.java
@@ -1,6 +1,11 @@
 package de.qucosa.oai.provider.persitence.exceptions;
 
+import java.sql.SQLException;
+
 public class NotFound extends Exception {
     public NotFound(String message) {
+    }
+
+    public NotFound(String s, SQLException e) {
     }
 }
