371840dec4d51e170120dc8df364e41c5bebcc21
dseelig <daniel.seelig@slub-dresden.de>
2018-09-13 09:38:58 +0200

    Add a second constructor with message and exception object parameter.

--- src/main/java/de/qucosa/oai/provider/persitence/exceptions/DeleteFailed.java
+++ src/main/java/de/qucosa/oai/provider/persitence/exceptions/DeleteFailed.java
@@ -1,6 +1,11 @@
 package de.qucosa.oai.provider.persitence.exceptions;
 
+import java.sql.SQLException;
+
 public class DeleteFailed extends Exception {
     public DeleteFailed(String s) {
+    }
+
+    public DeleteFailed(String s, SQLException e) {
     }
 }
