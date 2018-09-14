ec7125fad4a0579b122c6998eb3664a7aa6060f4
dseelig <daniel.seelig@slub-dresden.de>
2018-09-14 13:26:52 +0200

    Add new tests. Edit save ist not succesful test if failed record id.

--- src/test/java/de/qucosa/oai/provider/controller/DisseminationControllerTest.java
+++ src/test/java/de/qucosa/oai/provider/controller/DisseminationControllerTest.java
@@ -1,15 +1,18 @@
 package de.qucosa.oai.provider.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.qucosa.oai.provider.services.DisseminationService;
-import de.qucosa.oai.provider.services.FormatService;
 import de.qucosa.oai.provider.dao.DisseminationTestDao;
 import de.qucosa.oai.provider.dao.FormatTestDao;
 import de.qucosa.oai.provider.persistence.Dao;
+import de.qucosa.oai.provider.persistence.exceptions.NotFound;
 import de.qucosa.oai.provider.persistence.model.Dissemination;
 import de.qucosa.oai.provider.persistence.model.Format;
+import de.qucosa.oai.provider.services.DisseminationService;
+import de.qucosa.oai.provider.services.FormatService;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,11 +23,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import testdata.TestData;
 
 import java.io.IOException;
 import java.util.List;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -45,6 +50,9 @@ public class DisseminationControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws IOException {
@@ -90,9 +98,22 @@ public class DisseminationControllerTest {
     }
 
     @Test
-    public void Save_dissemintaion_not_successful() throws Exception {
+    public void Save_dissemintaion_not_successful__if_recordid_failed() throws Exception {
         Dissemination dissemination = disseminations.get(0);
         dissemination.setRecordId(null);
+
+        mvc.perform(post("/dissemination")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(dissemination)))
+                .andExpect(status().isNotAcceptable())
+                .andExpect(jsonPath("$.errorMsg", is("Cannot save dissemination.")))
+                .andExpect(jsonPath("$.statuscode", is("406")));
+    }
+
+    @Test
+    public void Save_dissemintaion_not_successful__if_formatid_failed() throws Exception {
+        Dissemination dissemination = disseminations.get(0);
+        dissemination.setFormatId(null);
 
         mvc.perform(post("/dissemination")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -111,11 +132,36 @@ public class DisseminationControllerTest {
     }
 
     @Test
+    public void Find_disseminations_by_uid_not_successful() throws Exception {
+        MvcResult mvcResult = mvc.perform(get("/dissemination/oai:example:org:qucosa:5588")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+        String res = mvcResult.getResponse().getContentAsString();
+        assertThat(res).isEmpty();
+    }
+
+    @Test
     public void Mark_disseminations_as_deleted() throws Exception {
         mvc.perform(delete("/dissemination/oai:example:org:qucosa:55887/xmetadiss/true")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.deleted", is(true)));
+    }
+
+    @Test
+    public void Mark_disseminations_as_deleted_not_successful_if_has_wrong_format() throws Exception {
+        mvc.perform(delete("/dissemination/oai:example:org:qucosa:55887/xmetaiss/true")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errorMsg", is("Cannot find format.")));
+    }
+
+    @Test
+    public void Mark_disseminations_as_deleted_not_successful_if_has_wrong_uid() throws Exception {
+        mvc.perform(delete("/dissemination/oai:example:org:qucosa:5887/xmetadiss/true")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errorMsg", is("Cannot find dissemination.")));
     }
 
     @Test
