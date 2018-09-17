4ac6207ff2efa5ce0f3c9b1514a1eb8887a1d2dc
dseelig <daniel.seelig@slub-dresden.de>
2018-09-17 10:13:43 +0200

    Optimize error details object. Remove alle public setters. Init properties in constructor. Add an seperate response entity method.

--- src/main/java/de/qucosa/oai/provider/ErrorDetails.java
+++ src/main/java/de/qucosa/oai/provider/ErrorDetails.java
@@ -1,116 +1,104 @@
 package de.qucosa.oai.provider;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import java.time.LocalDateTime;
-import java.util.Date;
 
 public class ErrorDetails {
     private String classname;
 
-    private String line;
-
-    private String statuscode;
+    private HttpStatus statuscode;
 
     private String errorMsg;
 
     private String requestPath;
 
-    private String exception;
+    private String requestMethod;
 
-    private String stacktrace;
+    private Exception exception;
+
+    private StackTraceElement[] stacktrace;
 
     private LocalDateTime date;
 
     private String method;
 
-    private String requestMethod;
+    public ErrorDetails create(String classname, String method, String requestMethodAndPath, HttpStatus statuscode, String errorMsg, Exception exception) {
+        this.date = LocalDateTime.now();
+        this.classname = classname;
+        this.method = method;
+        this.statuscode = statuscode;
+        this.errorMsg = (exception != null) ? exception.getMessage() : errorMsg;
+        setException(exception);
+        setRequestPath(requestMethodAndPath);
+        return this;
+    }
+
+    public ResponseEntity response() {
+        return new ResponseEntity(this, this.statuscode);
+    }
 
     public String getClassname() {
         return classname;
     }
 
-    public ErrorDetails setClassname(String classname) {
-        this.classname = classname;
-        return this;
-    }
-
-    public String getLine() {
-        return line;
-    }
-
-    public ErrorDetails setLine(String line) {
-        this.line = line;
-        return this;
-    }
-
     public String getStatuscode() {
-        return statuscode;
-    }
-
-    public ErrorDetails setStatuscode(String statuscode) {
-        this.statuscode = statuscode;
-        return this;
+        return statuscode.toString();
     }
 
     public String getErrorMsg() {
         return errorMsg;
     }
 
-    public ErrorDetails setErrorMsg(String errorMsg) {
-        this.errorMsg = errorMsg;
-        return this;
-    }
-
     public String getRequestPath() {
         return requestPath;
     }
 
-    public ErrorDetails setRequestPath(String requestPath) {
-        this.requestPath = requestPath;
-        return this;
-    }
-
-    public String getException() {
+    public Exception getException() {
         return exception;
     }
 
-    public ErrorDetails setException(String exception) {
-        this.exception = exception;
-        return this;
-    }
-
-    public String getStacktrace() {
+    public StackTraceElement[] getStacktrace() {
         return stacktrace;
-    }
-
-    public ErrorDetails setStacktrace(String stacktrace) {
-        this.stacktrace = stacktrace;
-        return this;
     }
 
     public LocalDateTime getDate() {
         return date;
     }
 
-    public ErrorDetails setDate(LocalDateTime date) {
-        this.date = date;
-        return this;
-    }
-
     public String getMethod() {
         return method;
-    }
-
-    public ErrorDetails setMethod(String method) {
-        this.method = method;
-        return this;
     }
 
     public String getRequestMethod() {
         return requestMethod;
     }
 
-    public ErrorDetails setRequestMethod(String requestMethod) {
-        this.requestMethod = requestMethod;
+    private ErrorDetails setMethod(String method) {
+        this.method = method;
+        return this;
+    }
+
+    private ErrorDetails setRequestPath(String requestMethodAndPath) {
+        String[] req = requestMethodAndPath.split(":");
+        this.requestPath = req[1];
+        this.requestMethod = req[0];
+        return this;
+    }
+
+    private ErrorDetails setException(Exception exception) {
+        this.exception = exception;
+        setStacktrace(this.exception.getStackTrace());
+        return this;
+    }
+    private ErrorDetails setStacktrace(StackTraceElement[] stacktrace) {
+        this.stacktrace = stacktrace;
+        return this;
+    }
+
+    private ErrorDetails setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
         return this;
     }
 }
