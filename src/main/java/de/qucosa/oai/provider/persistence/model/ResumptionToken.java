b0edb77ac399798520a769568c0c87cdbfd935eb
dseelig <daniel.seelig@slub-dresden.de>
2019-03-05 08:40:05 +0100

    Add data pojos for resumption token tables.

---
+++ src/main/java/de/qucosa/oai/provider/persistence/model/ResumptionToken.java
@@ -0,0 +1,74 @@
+/*
+ * *
+ *     ~ Copyright 2018 Saxon State and University Library Dresden (SLUB)
+ *     ~
+ *     ~ Licensed under the Apache License, Version 2.0 (the "License");
+ *     ~ you may not use this file except in compliance with the License.
+ *     ~ You may obtain a copy of the License at
+ *     ~
+ *     ~     http://www.apache.org/licenses/LICENSE-2.0
+ *     ~
+ *     ~ Unless required by applicable law or agreed to in writing, software
+ *     ~ distributed under the License is distributed on an "AS IS" BASIS,
+ *     ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     ~ See the License for the specific language governing permissions and
+ *     ~ limitations under the License.
+ *
+ */
+
+package de.qucosa.oai.provider.persistence.model;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@JsonAutoDetect
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResumptionToken implements Serializable, HasIdentifier {
+
+    @JsonProperty("tokenId")
+    private Long tokenId;
+
+    @JsonProperty("expirationDate")
+    private Timestamp expirationDate;
+
+    @JsonProperty("cursor")
+    private Long cursor;
+
+    @Override
+    public void setIdentifier(Object identifier) {
+        setTokenId(Long.valueOf(identifier.toString()));
+    }
+
+    @Override
+    public Object getIdentifier() {
+        return getIdentifier();
+    }
+
+    public Long getTokenId() {
+        return tokenId;
+    }
+
+    public void setTokenId(Long tokenId) {
+        this.tokenId = tokenId;
+    }
+
+    public Timestamp getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Timestamp expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Long getCursor() {
+        return cursor;
+    }
+
+    public void setCursor(Long cursor) {
+        this.cursor = cursor;
+    }
+}
