6c55cd36a6ce921d9dcda55e54d1889ec61005f3
dseelig <daniel.seelig@slub-dresden.de>
2019-03-05 08:47:38 +0100

    Add daos for resumption token tables.

---
+++ src/main/java/de/qucosa/oai/provider/persistence/dao/postgres/ResumptionTokenDao.java
@@ -0,0 +1,91 @@
+/*
+ * *
+ *     ~ Copyright 2018 Saxon State and University Library Dresden (SLUB)
+ *     ~
+ *     ~ Licensed under the Apache License, Version 2.0 (the "License");
+ *     ~ you may not use this file except in compliance with the License.
+ *     ~ You may obtain a copy of the License at
+ *     ~
+ *     ~     http://www.apache.org/licenses/LICENSE-2.0
+ *     ~
+ *     ~ Unless required by applicable law or agreed to in writing, software
+ *     ~ distributed under the License is distributed on an "AS IS" BASIS,
+ *     ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     ~ See the License for the specific language governing permissions and
+ *     ~ limitations under the License.
+ *
+ */
+
+package de.qucosa.oai.provider.persistence.dao.postgres;
+
+import de.qucosa.oai.provider.persistence.Dao;
+import de.qucosa.oai.provider.persistence.exceptions.DeleteFailed;
+import de.qucosa.oai.provider.persistence.exceptions.NotFound;
+import de.qucosa.oai.provider.persistence.exceptions.SaveFailed;
+import de.qucosa.oai.provider.persistence.exceptions.UndoDeleteFailed;
+import de.qucosa.oai.provider.persistence.exceptions.UpdateFailed;
+import de.qucosa.oai.provider.persistence.model.ResumptionToken;
+
+import java.util.Collection;
+
+public class ResumptionTokenDao<T extends ResumptionToken> implements Dao<T> {
+    @Override
+    public T saveAndSetIdentifier(T object) throws SaveFailed {
+        return null;
+    }
+
+    @Override
+    public Collection<T> saveAndSetIdentifier(Collection<T> objects) throws SaveFailed {
+        return null;
+    }
+
+    @Override
+    public T update(T object) throws UpdateFailed {
+        return null;
+    }
+
+    @Override
+    public Collection<T> update(Collection<T> objects) throws UpdateFailed {
+        return null;
+    }
+
+    @Override
+    public Collection<T> findAll() throws NotFound {
+        return null;
+    }
+
+    @Override
+    public T findById(String id) throws NotFound {
+        return null;
+    }
+
+    @Override
+    public Collection<T> findByPropertyAndValue(String property, String value) throws NotFound {
+        return null;
+    }
+
+    @Override
+    public T findByMultipleValues(String clause, String... values) throws NotFound {
+        return null;
+    }
+
+    @Override
+    public void delete(String ident) throws DeleteFailed {
+
+    }
+
+    @Override
+    public void undoDelete(String ident) throws UndoDeleteFailed {
+
+    }
+
+    @Override
+    public void delete(T object) throws DeleteFailed {
+
+    }
+
+    @Override
+    public void undoDelete(T object) throws UndoDeleteFailed {
+
+    }
+}
