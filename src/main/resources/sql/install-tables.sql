bffba29786e8e7f6b83bd2d6b50ea859c8a4b826
dseelig <daniel.seelig@slub-dresden.de>
2019-03-11 17:55:46 +0100

    DB table installer.

---
+++ src/main/resources/sql/install-tables.sql
@@ -0,0 +1,136 @@
+﻿-- Table: public.sets
+-- DROP TABLE public.sets;
+CREATE TABLE public.sets
+(
+  id bigint NOT NULL,
+  setspec character varying(100) NOT NULL,
+  setname character varying(100) NOT NULL,
+  setdescription character varying(500),
+  deleted boolean NOT NULL DEFAULT false,
+  CONSTRAINT sets_pkey PRIMARY KEY (id),
+  CONSTRAINT setspec_unique UNIQUE (setspec)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.sets
+  OWNER TO postgres;
+
+
+-- Table: public.formats
+-- DROP TABLE public.formats;
+CREATE TABLE public.formats
+(
+  id bigint NOT NULL,
+  mdprefix character varying(255) NOT NULL,
+  schemaurl character varying(255) NOT NULL,
+  namespace character varying(255) NOT NULL,
+  deleted boolean NOT NULL DEFAULT false,
+  CONSTRAINT formats_pkey PRIMARY KEY (id),
+  CONSTRAINT mdprefix_unique UNIQUE (mdprefix)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.formats
+  OWNER TO postgres;
+
+
+-- Table: public.records
+-- DROP TABLE public.records;
+CREATE TABLE public.records
+(
+  id bigint NOT NULL,
+  pid character varying(50) NOT NULL,
+  uid character varying(100) NOT NULL,
+  deleted boolean NOT NULL DEFAULT false,
+  CONSTRAINT record_pkey PRIMARY KEY (id),
+  CONSTRAINT record_unique UNIQUE (pid),
+  CONSTRAINT record_uid_unique UNIQUE (uid)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.records
+  OWNER TO postgres;
+
+
+-- Table: public.disseminations
+-- DROP TABLE public.disseminations;
+CREATE TABLE public.disseminations
+(
+  id bigint NOT NULL,
+  id_format bigint NOT NULL,
+  lastmoddate timestamp with time zone,
+  xmldata xml NOT NULL,
+  deleted boolean NOT NULL DEFAULT false,
+  id_record character varying(100) NOT NULL,
+  CONSTRAINT dissemination_pkey PRIMARY KEY (id),
+  CONSTRAINT dissemination_format_fkey FOREIGN KEY (id_format)
+      REFERENCES public.formats (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT dissemination_record_fkey FOREIGN KEY (id_record)
+      REFERENCES public.records (uid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.disseminations
+  OWNER TO postgres;
+
+
+-- Table: public.sets_to_records
+-- DROP TABLE public.sets_to_records;
+CREATE TABLE public.sets_to_records
+(
+  id_set bigint NOT NULL,
+  id_record bigint NOT NULL,
+  CONSTRAINT str_record_fkey FOREIGN KEY (id_record)
+      REFERENCES public.records (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT str_set_fkey FOREIGN KEY (id_set)
+      REFERENCES public.sets (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.sets_to_records
+OWNER TO postgres;
+
+
+-- Table: public.resumption_tokens
+-- DROP TABLE public.resumption_tokens;
+CREATE TABLE public.resumption_tokens
+(
+  token_id bigint NOT NULL,
+  expiration_date timestamp with time zone NOT NULL,
+  cursor bigint NOT NULL,
+  CONSTRAINT pk_token_id PRIMARY KEY (token_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.resumption_tokens
+  OWNER TO postgres;
+
+
+-- Table: public.rst_to_identifiers
+-- DROP TABLE public.rst_to_identifiers;
+CREATE TABLE public.rst_to_identifiers
+(
+  rst_id bigint NOT NULL,
+  record_id bigint NOT NULL,
+  CONSTRAINT fk_record_id FOREIGN KEY (record_id)
+      REFERENCES public.records (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_rst_id FOREIGN KEY (rst_id)
+      REFERENCES public.resumption_tokens (token_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.rst_to_identifiers
+  OWNER TO postgres;
