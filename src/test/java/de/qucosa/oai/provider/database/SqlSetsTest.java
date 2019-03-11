5c967e2b6c61ed0a67bd7a5c8ae64aa94bd33c93
dseelig <daniel.seelig@slub-dresden.de>
2019-03-11 17:56:26 +0100

    Embedded postgres test.

---
+++ src/test/java/de/qucosa/oai/provider/database/SqlSetsTest.java
@@ -0,0 +1,44 @@
+/*
+ * *
+ *     ~ Copyright 2018 Saxon State and University Library Dresden (SLUB)
+ *     ~
+ *     ~ Licensed under the Apache License, Version 2.0 (the "License");
+ *     ~ you may not use this file except in compliance with the License.
+ *     ~ You may obtain a copy of the License at
+ *     ~
+ *     ~     http://www.apache.org/licenses/LICENSE-2.0
+ *     ~
+ *     ~ Unless required by applicable law or agreed to in writing, software
+ *     ~ distributed under the License is distributed on an "AS IS" BASIS,
+ *     ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     ~ See the License for the specific language governing permissions and
+ *     ~ limitations under the License.
+ *
+ */
+
+package de.qucosa.oai.provider.database;
+
+import de.qucosa.oai.provider.persistence.Dao;
+import de.qucosa.oai.provider.persistence.dao.postgres.SetDao;
+import de.qucosa.oai.provider.persistence.exceptions.NotFound;
+import de.qucosa.oai.provider.persistence.model.Set;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class SqlSetsTest extends EmbeddedPsqlConnector {
+    @Test
+    public void Find_all_sets() throws SQLException, NotFound {
+        Dao<Set> dao = new SetDao<Set>(connection());
+        Collection<Set> sets = dao.findAll();
+        assertThat(sets.size() ).isGreaterThan(0);
+        connection().close();
+        postgres().stop();
+    }
+}
