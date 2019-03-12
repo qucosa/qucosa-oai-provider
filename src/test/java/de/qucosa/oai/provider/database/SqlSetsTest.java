1ea91223d203cdb39450aa2bc3ada1b12d705ba5
dseelig <daniel.seelig@slub-dresden.de>
2019-03-12 08:12:11 +0100

    Add test method ordering. Expand class with abstract embedded postgres class.

--- src/test/java/de/qucosa/oai/provider/database/SqlSetsTest.java
+++ src/test/java/de/qucosa/oai/provider/database/SqlSetsTest.java
@@ -22,52 +22,23 @@ import de.qucosa.oai.provider.persistence.Dao;
 import de.qucosa.oai.provider.persistence.dao.postgres.SetDao;
 import de.qucosa.oai.provider.persistence.exceptions.NotFound;
 import de.qucosa.oai.provider.persistence.model.Set;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
+import org.junit.jupiter.api.TestMethodOrder;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Collection;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static ru.yandex.qatools.embed.postgresql.distribution.Version.Main.V9_5;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class SqlSetsTest {
-
-    private EmbeddedPostgres postgres;
-
-    private Connection connection;
-
-    private String connUrl;
-
-    @BeforeAll
-    public void setUp() throws IOException, SQLException {
-        postgres = new EmbeddedPostgres(V9_5);
-        connUrl = postgres.start("localhost", 5432,  "oaiprovider",
-                "postgres", "postgres");
-        connection = DriverManager.getConnection(connUrl);
-        postgres.getProcess().get().importFromFile(
-                new File(getClass().getResource("/database/psql-oia-provider-test-data.sql").getPath()));
-
-        System.out.println("init global for all tests.");
-    }
-
-    @AfterAll
-    public void destroy() throws SQLException {
-        connection.close();
-        postgres.stop();
-        System.out.println("destroy all inits.");
-    }
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SqlSetsTest extends EmbeddedPostgresForUnittests {
 
     @Test
+    @Order(1)
     public void Find_all_sets() throws SQLException, NotFound {
         Dao<Set> dao = new SetDao<Set>(connection);
         Collection<Set> sets = dao.findAll();
@@ -77,6 +48,7 @@ public class SqlSetsTest {
     }
 
     @Test
+    @Order(2)
     public void Find_set_by_setspec() throws NotFound {
         Dao<Set> dao = new SetDao<Set>(connection);
         Collection<Set> sets = dao.findByPropertyAndValue("setspec","ddc:610");
