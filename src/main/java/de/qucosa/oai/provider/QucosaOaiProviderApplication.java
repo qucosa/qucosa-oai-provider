cfc65e7dc11b387e86badd2a45f117eb9e6d3517
dseelig <daniel.seelig@slub-dresden.de>
2019-03-12 15:28:35 +0100

    Remove unused code.

--- src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
+++ src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
@@ -15,22 +15,13 @@
  */
 package de.qucosa.oai.provider;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
-@ImportResource({"classpath:applicationContext.xml"})
-public class QucosaOaiProviderApplication implements CommandLineRunner {
+public class QucosaOaiProviderApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(QucosaOaiProviderApplication.class, args);
+        SpringApplication.run(QucosaOaiProviderApplication.class, args);
     }
-
-    @Override
-    public void run(String... args) { }
 }
