aac4d9a69e623034507fe151a61cc501e8e7b067
dseelig <daniel.seelig@slub-dresden.de>
2019-03-12 15:28:53 +0100

    Add find all service method.

--- src/main/java/de/qucosa/oai/provider/controller/RecordController.java
+++ src/main/java/de/qucosa/oai/provider/controller/RecordController.java
@@ -38,6 +38,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -275,6 +276,21 @@ public class RecordController {
         }
 
         return new ResponseEntity(true, HttpStatus.OK);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity findAll() {
+        Collection<Record> records = null;
+
+        try {
+            records = recordService.findAll();
+        } catch (NotFound notFound) {
+            return new ErrorDetails(this.getClass().getName(), "findAll", "GET:find}",
+                    HttpStatus.NOT_FOUND, null, notFound).response();
+        }
+
+        return new ResponseEntity(records, HttpStatus.OK);
     }
 
     @RequestMapping(value = "{uid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
