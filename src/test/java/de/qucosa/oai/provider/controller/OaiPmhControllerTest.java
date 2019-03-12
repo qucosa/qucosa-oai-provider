20b0bbb28395ef724734d117c2074bb28731ee7b
dseelig <daniel.seelig@slub-dresden.de>
2019-03-12 15:22:10 +0100

    Add oai pmh controller test class.

---
+++ src/test/java/de/qucosa/oai/provider/controller/OaiPmhControllerTest.java
@@ -0,0 +1,61 @@
+/*
+ * *
+ *     ~ Copyright 2018 Saxon State and University Library Dresden (SLUB)
+ *     ~
+ *     ~ Licensed under the Apache License, Version 2.0 (the "License");
+ *     ~ you may not use this file except in compliance with the License.
+ *     ~ You may obtain a copy of the License at
+ *     ~
+ *     ~     http://www.apache.org/licenses/LICENSE-2.0
+ *     ~
+ *     ~ Unless required by applicable law or agreed to in writing, software
+ *     ~ distributed under the License is distributed on an "AS IS" BASIS,
+ *     ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     ~ See the License for the specific language governing permissions and
+ *     ~ limitations under the License.
+ *
+ */
+
+package de.qucosa.oai.provider.controller;
+
+import de.qucosa.oai.provider.QucosaOaiProviderApplication;
+import de.qucosa.oai.provider.OaiPmhTestApplicationConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {QucosaOaiProviderApplication.class, OaiPmhTestApplicationConfig.class})
+@AutoConfigureMockMvc
+public class OaiPmhControllerTest {
+    private Logger logger = LoggerFactory.getLogger(OaiPmhControllerTest.class);
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Find all formats for build oai pmh identifiers list in xml.")
+    public void findAllFormats() throws Exception {
+        MvcResult mvcResult = mvc.perform(
+                get("/oai/ListIdentifiers/oai_dc")
+                .contentType(MediaType.APPLICATION_XML_VALUE))
+                .andExpect(status().isOk()).andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+        assertThat(content).isNotEmpty();
+    }
+}
