8acb98e71663a65b7fe09fbf0048e7e1ca8a57e2
dseelig <daniel.seelig@slub-dresden.de>
2019-03-19 16:01:59 +0100

    Add service for oai pmh lists view.

---
+++ src/main/java/de/qucosa/oai/provider/services/views/OaiPmhListsService.java
@@ -0,0 +1,37 @@
+/*
+ * *
+ *     ~ Copyright 2018 Saxon State and University Library Dresden (SLUB)
+ *     ~
+ *     ~ Licensed under the Apache License, Version 2.0 (the "License");
+ *     ~ you may not use this file except in compliance with the License.
+ *     ~ You may obtain a copy of the License at
+ *     ~
+ *     ~     http://www.apache.org/licenses/LICENSE-2.0
+ *     ~
+ *     ~ Unless required by applicable law or agreed to in writing, software
+ *     ~ distributed under the License is distributed on an "AS IS" BASIS,
+ *     ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     ~ See the License for the specific language governing permissions and
+ *     ~ limitations under the License.
+ *
+ */
+
+package de.qucosa.oai.provider.services.views;
+
+import de.qucosa.oai.provider.persistence.Dao;
+import de.qucosa.oai.provider.persistence.exceptions.NotFound;
+import de.qucosa.oai.provider.persistence.model.views.OaiPmhLists;
+
+public class OaiPmhListsService<T> {
+    private Dao dao;
+
+    public OaiPmhListsService(){}
+
+    public void setDao(Dao dao) {
+        this.dao = dao;
+    }
+
+    public OaiPmhLists findByMultipleValues(String clause, String... values) throws NotFound {
+        return (OaiPmhLists) dao.findByMultipleValues(clause, values);
+    }
+}
