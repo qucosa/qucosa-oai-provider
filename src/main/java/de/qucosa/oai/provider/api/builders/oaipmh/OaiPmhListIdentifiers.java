249efb5f69cab9ed05e0679623db4bd5a9c17cdb
dseelig <daniel.seelig@slub-dresden.de>
2019-03-21 09:21:25 +0100

    Outsorce build list header in abstract class OaiPmhList.

--- src/main/java/de/qucosa/oai/provider/api/builders/oaipmh/OaiPmhListIdentifiers.java
+++ src/main/java/de/qucosa/oai/provider/api/builders/oaipmh/OaiPmhListIdentifiers.java
@@ -18,18 +18,10 @@
 
 package de.qucosa.oai.provider.api.builders.oaipmh;
 
-import de.qucosa.oai.provider.api.utils.DateTimeConverter;
-import de.qucosa.oai.provider.api.utils.DocumentXmlUtils;
 import de.qucosa.oai.provider.persistence.exceptions.NotFound;
-import de.qucosa.oai.provider.persistence.model.Dissemination;
-import de.qucosa.oai.provider.persistence.model.Record;
-import de.qucosa.oai.provider.persistence.model.Set;
-import de.qucosa.oai.provider.persistence.model.views.OaiPmhLists;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-
-import java.util.Collection;
 
 public class OaiPmhListIdentifiers extends OaiPmhList implements OaiPmhListBuilder {
 
@@ -48,57 +40,13 @@ public class OaiPmhListIdentifiers extends OaiPmhList implements OaiPmhListBuild
     }
 
     private void buildIdentirierList() throws NotFound {
-        Node listIdentifiers = oaiPmhTemplate.getElementsByTagName(verb).item(0);
 
         if (oaiPmhLists == null || oaiPmhLists.isEmpty()) {
-            int i = 0;
-
-            for (Record record : records) {
-                i++;
-                record = records.iterator().next();
-
-                Collection<Set> sets = setsToRecordService.findByPropertyAndValue("rc.id", String.valueOf(record.getIdentifier()));
-
-                Dissemination dissemination = disseminationService.findByMultipleValues(
-                        "id_format = %s AND id_record = %s", String.valueOf(format.getIdentifier()), record.getUid());
-
-                Document identifierTpl = DocumentXmlUtils.document(
-                        getClass().getResourceAsStream("/templates/identifier.xml"), true);
-                Node identifier = identifierTpl.getElementsByTagName("identifier").item(0);
-                identifier.setTextContent(record.getUid());
-
-                Node datestamp = identifierTpl.getElementsByTagName("datestamp").item(0);
-                datestamp.setTextContent(dissemination.getLastmoddate().toString());
-
-                writeSetsInHeader(identifierTpl, sets);
-
-                listIdentifiers.appendChild(oaiPmhTemplate.importNode(identifierTpl.getDocumentElement(), true));
-
-                if (i == recordsProPage) {
-                    break;
-                }
-            }
-
+            createListHeader(oaiPmhTemplate.getElementsByTagName(verb).item(0),
+                    records);
         } else {
-
-            for (OaiPmhLists entry : oaiPmhLists) {
-                Collection<Set> sets = setsToRecordService.findByPropertyAndValue("rc.id", String.valueOf(entry.getRecordId()));
-                Dissemination dissemination = disseminationService.findByMultipleValues(
-                        "id_format = %s AND id_record = %s", String.valueOf(format.getIdentifier()), entry.getUid());
-
-                Document identifierTpl = DocumentXmlUtils.document(
-                        getClass().getResourceAsStream("/templates/identifier.xml"), true);
-
-                Node identifier = identifierTpl.getElementsByTagName("identifier").item(0);
-                identifier.setTextContent(entry.getUid());
-
-                Node datestamp = identifierTpl.getElementsByTagName("datestamp").item(0);
-                datestamp.setTextContent(dissemination.getLastmoddate().toString());
-
-                writeSetsInHeader(identifierTpl, sets);
-
-                listIdentifiers.appendChild(oaiPmhTemplate.importNode(identifierTpl.getDocumentElement(), true));
-            }
+            createListHeader(oaiPmhTemplate.getElementsByTagName(verb).item(0),
+                    oaiPmhLists);
         }
 
         if (resumptionToken != null) {
