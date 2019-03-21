cd7277a3c7a45c0a2cb76fcfda34280b23963ff7
dseelig <daniel.seelig@slub-dresden.de>
2019-03-21 14:12:03 +0100

    Implements list build for oai records.

--- src/main/java/de/qucosa/oai/provider/api/builders/oaipmh/OaiPmhListRecords.java
+++ src/main/java/de/qucosa/oai/provider/api/builders/oaipmh/OaiPmhListRecords.java
@@ -18,7 +18,20 @@
 
 package de.qucosa.oai.provider.api.builders.oaipmh;
 
+import de.qucosa.oai.provider.api.utils.DocumentXmlUtils;
+import de.qucosa.oai.provider.persistence.exceptions.NotFound;
+import de.qucosa.oai.provider.persistence.model.Record;
+import de.qucosa.oai.provider.persistence.model.views.OaiPmhLists;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
 
 public class OaiPmhListRecords extends OaiPmhList implements OaiPmhListBuilder {
 
@@ -27,6 +40,57 @@ public class OaiPmhListRecords extends OaiPmhList implements OaiPmhListBuilder {
     }
 
     @Override
-    public void list() {
+    public void list() throws NotFound, UnsupportedEncodingException {
+        listNode();
+
+        if (oaiPmhLists == null || oaiPmhLists.isEmpty()) {
+            addList(records);
+        } else {
+            addList(oaiPmhLists);
+        }
+    }
+
+    private void addList(Collection collection) throws NotFound, UnsupportedEncodingException {
+
+        for (Object object : collection) {
+            Document entry = DocumentXmlUtils.document(
+                    getClass().getResourceAsStream("/templates/record.xml"), true);
+            Node metadata = entry.getElementsByTagName("metadata").item(0);
+            InputStream is = null;
+
+            if (object instanceof OaiPmhLists) {
+
+                if (!((OaiPmhLists) object).isRecordStatus()) {
+                    is = new ByteArrayInputStream(dissemination(
+                            "id_format = %s AND id_record = %s", String.valueOf(format.getIdentifier()),
+                            ((OaiPmhLists) object).getUid()).getXmldata().getBytes("UTF-8"));
+                }
+            } else if (object instanceof Record) {
+
+                if (!((Record) object).isDeleted()) {
+                    is = new ByteArrayInputStream(dissemination(
+                            "id_format = %s AND id_record = %s", String.valueOf(format.getIdentifier()),
+                            ((Record) object).getUid()).getXmldata().getBytes("UTF-8"));
+                }
+            }
+
+            if (is != null) {
+                Document verbXml = DocumentXmlUtils.document(is, true);
+                Node importedMetadata = entry.importNode(verbXml.getDocumentElement(), true);
+                metadata.appendChild(importedMetadata);
+            }
+
+            Node recordNode = entry.getElementsByTagName("record").item(0);
+            Node importedHeader = entry.importNode(addHeaderTpl(object).getDocumentElement(), true);
+            recordNode.insertBefore(importedHeader, metadata);
+
+            Element verbElem = (Element) oaiPmhTemplate.getElementsByTagName(verb).item(0);
+            Node imported = oaiPmhTemplate.importNode(entry.getDocumentElement(), true);
+            verbElem.appendChild(imported);
+        }
+
+        if (resumptionToken != null) {
+            addResumtionToken(records.size());
+        }
     }
 }
