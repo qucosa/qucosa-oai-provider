f980b864c4266d5fb3bf57e3b6147fb32c77bc21
dseelig <daniel.seelig@slub-dresden.de>
2019-03-25 10:47:04 +0100

    Add EnableScheduled annotation.

--- src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
+++ src/main/java/de/qucosa/oai/provider/QucosaOaiProviderApplication.java
@@ -17,8 +17,10 @@ package de.qucosa.oai.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class QucosaOaiProviderApplication {
 
     public static void main(String[] args) {
