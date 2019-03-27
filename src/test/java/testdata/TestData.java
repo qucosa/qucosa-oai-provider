e6e63be74e2580444ba52b159b02732ae3933bd9
dseelig <daniel.seelig@slub-dresden.de>
2019-03-27 14:23:01 +0100

    Remove ids from formats data.

--- src/test/java/testdata/TestData.java
+++ src/test/java/testdata/TestData.java
@@ -20,9 +20,9 @@ public class TestData {
     public static String SETS = "[{\"setspec\": \"ddc:1200\", \"setname\": \"Test Set 1200\", \"setdescription\" : \"\"}," +
             "{\"setspec\": \"ddc:1201\", \"setname\": \"Test Set 1201\", \"setdescription\" : \"\"}]";
 
-    public static String FORMATS = "[{\"formatId\" : 1, \"mdprefix\" : \"oai_dc\", \"schemaurl\" : \"http://www.openarchives.org/OAI/2.0/oai_dc/\", \"namespace\" : \"oai_dc\", \"deleted\" : \"false\"}," +
-            "{\"formatId\" : 2, \"mdprefix\" : \"xmetadiss\", \"schemaurl\" : \"http://www.d-nb.de/standards/xmetadissplus/\", \"namespace\" : \"xmetadiss\", \"deleted\" : \"false\"}," +
-            "{\"formatId\" : 3, \"mdprefix\" : \"epicur\", \"schemaurl\" : \"http://www.persistent-identifier.de/xepicur/version1.0/xepicur.xsd\", \"namespace\" : \"epicur\", \"deleted\" : \"false\"}]";
+    public static String FORMATS = "[{\"mdprefix\" : \"oai_dc\", \"schemaurl\" : \"http://www.openarchives.org/OAI/2.0/oai_dc/\", \"namespace\" : \"oai_dc\", \"deleted\" : \"false\"}," +
+            "{\"mdprefix\" : \"xmetadiss\", \"schemaurl\" : \"http://www.d-nb.de/standards/xmetadissplus/\", \"namespace\" : \"xmetadiss\", \"deleted\" : \"false\"}," +
+            "{\"mdprefix\" : \"epicur\", \"schemaurl\" : \"http://www.persistent-identifier.de/xepicur/version1.0/xepicur.xsd\", \"namespace\" : \"epicur\", \"deleted\" : \"false\"}]";
 
     public static String RECORDS = "[{\"pid\" : \"qucosa:55887\", \"uid\" : \"oai:example:org:qucosa:55887\"}, " +
             "{\"pid\" : \"qucosa:57777\", \"uid\" : \"oai:example:org:qucosa:57777\"}," +
