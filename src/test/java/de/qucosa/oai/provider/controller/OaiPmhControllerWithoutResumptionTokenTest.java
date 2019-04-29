80e9a8dcd6779665caf054051d6677084f9dc5a7
dseelig <daniel.seelig@slub-dresden.de>
2019-04-29 09:51:11 +0200

    Remove ListMetadataFormtas test.

--- src/test/java/de/qucosa/oai/provider/controller/OaiPmhControllerWithoutResumptionTokenTest.java
+++ src/test/java/de/qucosa/oai/provider/controller/OaiPmhControllerWithoutResumptionTokenTest.java
@@ -148,24 +148,6 @@ public class OaiPmhControllerWithoutResumptionTokenTest {
     }
 
     @Test
-    @DisplayName("Returns list of formats.")
-    public void getListMetadataFormats() throws Exception {
-        MvcResult mvcResult = mvc.perform(
-                get("/oai/ListMetadataFormats")
-                        .contentType(MediaType.APPLICATION_XML_VALUE))
-                .andExpect(status().isOk()).andReturn();
-        String response = mvcResult.getResponse().getContentAsString();
-        assertThat(response).isNotEmpty();
-
-        Document document = DocumentXmlUtils.document(
-                new ByteArrayInputStream(response.getBytes("UTF-8")), true);
-        assertThat(document).isNotNull();
-
-        Node listMetadataFormats = document.getElementsByTagName("ListMetadataFormats").item(0);
-        assertThat(listMetadataFormats.getNodeName()).isEqualTo("ListMetadataFormats");
-    }
-
-    @Test
     @DisplayName("Return identify configuration xml.")
     public void getIdentify() throws Exception {
         MvcResult mvcResult = mvc.perform(
