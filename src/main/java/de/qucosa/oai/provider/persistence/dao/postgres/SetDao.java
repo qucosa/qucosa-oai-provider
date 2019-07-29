10a1abce3c989891cfd851a7b8c88dae141a9378
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2019-07-29 14:18:37 +0200

    Remove unnecessary imports

--- src/main/java/de/qucosa/oai/provider/persistence/dao/postgres/SetDao.java
+++ src/main/java/de/qucosa/oai/provider/persistence/dao/postgres/SetDao.java
@@ -31,7 +31,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 @Repository
