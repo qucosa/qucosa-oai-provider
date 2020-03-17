12fe02cc97c9b081dc5e35eb830ee839fc36e569
dseelig <daniel.seelig@slub-dresden.de>
2020-03-17 20:38:47 +0100

    Add test for is valid schema test.

--- src/test/java/de/qucosa/oai/xml/XmlValidatorTest.java
+++ src/test/java/de/qucosa/oai/xml/XmlValidatorTest.java
@@ -20,17 +20,34 @@ package de.qucosa.oai.xml;
 
 import de.qucosa.oai.provider.api.exceptions.XmlDomParserException;
 import de.qucosa.oai.provider.api.utils.DocumentXmlUtils;
+import de.qucosa.oai.provider.api.validators.xml.XmlSchemaValidator;
+import de.qucosa.oai.provider.config.json.XmlNamespacesConfig;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
+import javax.xml.xpath.XPathExpressionException;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class XmlValidatorTest {
     private String testXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
             "<test><hello></hello><welt /><msg></test>";
+
+    private XmlNamespacesConfig namespacesConfig;
+
+    private XmlSchemaValidator schemaValidator;
+
+    @BeforeAll
+    public void init() throws IOException {
+        namespacesConfig = new XmlNamespacesConfig(getClass().getResourceAsStream("/config/namespaces.json"));
+        schemaValidator = new XmlSchemaValidator(namespacesConfig);
+    }
 
     @Test
     @DisplayName("Test has xml formed errors.")
@@ -39,5 +56,15 @@ public class XmlValidatorTest {
             DocumentXmlUtils.document(new ByteArrayInputStream(testXml.getBytes("UTF-8")),
                 true);
         });
+    }
+
+    @Test
+    public void isValidOaiDc() throws XmlDomParserException, XPathExpressionException {
+        Document doc = DocumentXmlUtils.document(
+                getClass().getResourceAsStream("/xml/oai-dc-test.xml"), true);
+        schemaValidator.setXmlDoc(doc);
+
+        Assertions.assertNotNull(schemaValidator.getXmlNode());
+        Assertions.assertTrue(schemaValidator.isValid());
     }
 }
