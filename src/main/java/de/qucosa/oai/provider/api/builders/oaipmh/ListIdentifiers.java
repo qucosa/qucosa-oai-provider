f24b6d7f6a9285ade02a1f3f49970ea33549b10f
dseelig <daniel.seelig@slub-dresden.de>
2020-12-03 12:13:24 +0100

    Change property name.

--- src/main/java/de/qucosa/oai/provider/api/builders/oaipmh/ListIdentifiers.java
+++ src/main/java/de/qucosa/oai/provider/api/builders/oaipmh/ListIdentifiers.java
@@ -62,7 +62,7 @@ public class ListIdentifiers extends OaiPmhDataBuilderAbstract implements OaiPmh
     private void buildListWithResumptionToken(Node verbNode) throws XmlDomParserException {
 
         for (OaiPmhListByToken obj : oaiPmhListByToken) {
-            Node importedHeader = oaiPmhTpl.importNode(addHeaderTpl(obj.getUid(),
+            Node importedHeader = oaiPmhTpl.importNode(addHeaderTpl(obj.getOaiid(),
                     DateTimeConverter.sqlTimestampToString(obj.getLastModDate()), obj.isRecordStatus(), obj.getSets()).getDocumentElement(), true);
             verbNode.appendChild(importedHeader);
         }
